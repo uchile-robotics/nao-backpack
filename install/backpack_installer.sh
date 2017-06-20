1574db70e3d3640bb001d5470abae6502e03b091
Matias Mattamala <mmattamala@ing.uchile.cl>
2017-06-20 15:52:16 -0400

    Updating backpack installer and some dependencies

--- install/backpack_installer.sh
+++ install/backpack_installer.sh
@@ -7,6 +7,8 @@ export CATKIN_WS=~/catkin_ws
 mkdir -p ${CATKIN_WS}/src
 cd ${CATKIN_WS}
 
+catkin_make -j1
+
 # install general ros dependencies
 sudo apt-get install ros-kinetic-cv-bridge ros-kinetic-image-transport ros-kinetic-tf ros-kinetic-sensor-msgs -y
 sudo apt-get install ros-kinetic-camera-calibration -y
@@ -17,8 +19,7 @@ wstool init src ${CURR_PATH}/nao_install.rosinstall
 # Install system and ROS dependencies
 rosdep install -i -y --from-paths src
 
-#catkin build --summarize -j1
-catkin_make -j1
-
 echo "source ${CATKIN_WS}/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+catkin_make -j1
